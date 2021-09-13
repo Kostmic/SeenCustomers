fc5d8a73d39339db501c5ff9a93942abb7316ccd
Kostmic <44363586+Kostmic@users.noreply.github.com>
2021-09-13 20:34:27 +0200

    Add id attribute to customer && add customers mem DB

--- backend/structs.go
+++ backend/structs.go
@@ -1,6 +1,7 @@
 package main
 
 type Customer struct {
+	Id      string `json:"id"`
 	Email   string `json:"title"`
 	PhoneNr string `json:"phonenr"`
 	Views   int    `json:"views"`
@@ -9,4 +10,27 @@ type Customer struct {
 type Event struct {
 	PageHits   int `json:"pagehits"`
 	VideoPlays int `json:"videoplays"`
+}
+
+type allCustomers []Customer
+
+var customers = allCustomers{
+	{
+		Id:      "1",
+		Email:   "john.eric@gmail.com",
+		PhoneNr: "47391048",
+		Views:   3,
+	},
+	{
+		Id:      "2",
+		Email:   "sarah_simpleton@hotmail.com",
+		PhoneNr: "38593926",
+		Views:   1,
+	},
+	{
+		Id:      "3",
+		Email:   "ChrisOlafson@gmail.com",
+		PhoneNr: "19573628",
+		Views:   8,
+	},
 }
