6bbd355412f8b2931a1d98ef59239947d540f35b
Kostmic <44363586+Kostmic@users.noreply.github.com>
2021-09-14 00:34:38 +0200

    Add working CRUD functionality
    📢 Add error handling for GetSingleUserById
    
    ✅ Add logic for create user
    ✅ Add logic for update user
    ✅ Add logic for delete user

--- backend/handlers.go
+++ backend/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -12,20 +13,69 @@ func GetAllUsers(writer http.ResponseWriter, request *http.Request){
 }
 func GetSingleUserById(writer http.ResponseWriter, request *http.Request){
 	params := mux.Vars(request)
+	//Check id from request with customers in "DB"
 	for _, customer := range customers {
-		if customer.Id == params["id"] {
+		if (customer.Id.String() == params["id"]) {
 			json.NewEncoder(writer).Encode(customer)
+			return
 		}
 	}
+	//Return 402 no content if Id does not exist
+	writer.WriteHeader(http.StatusNotFound) //404
 
 }
 func CreateUser(writer http.ResponseWriter, request *http.Request){
+	var customer Customer
+
+	err := json.NewDecoder(request.Body).Decode(&customer)
+	if err != nil{
+		http.Error(writer, err.Error(), http.StatusBadRequest) //400
+		return
+	}
+	//Generate unique id(UUID) for each customer
+	customer.Id = uuid.New()
+
+	customers = append(customers, customer)
+	json.NewEncoder(writer).Encode(&customer)
 
 }
 
 func UpdateUser(writer http.ResponseWriter, request *http.Request){
+	var updCustomer Customer
+	params := mux.Vars(request)
 
+	
+	//Check id from request with customers in "DB"
+	for i := 0; i < len(customers); i++ {
+		//Break out of loop if id is found
+		if (customers[i].Id.String() == params["id"]) {
+			json.NewEncoder(writer).Encode(customers[i])
+			err := json.NewDecoder(request.Body).Decode(&updCustomer)
+			if err != nil{
+				http.Error(writer, err.Error(), http.StatusBadRequest) //400
+				return
+			}
+			customers[i].Email = updCustomer.Email
+			customers[i].Phone = updCustomer.Phone
+			customers[i].Views = updCustomer.Views
+			return
+		}
+	}
+
+	//If ID isn't found return 404
+		http.Error(writer, "DENIED_ACCESS", http.StatusNotFound) //404
 }
 func DeleteUser(writer http.ResponseWriter, request *http.Request){
+	params := mux.Vars(request)
+	//Check id from request with customers in "DB"
+	for i := 0; i < len(customers); i++ {
+		//Break out of loop if id is found
+		if (customers[i].Id.String() == params["id"]) {
+			customers = append(customers[:i], customers[i+1:]...)   // Truncate slice.
+			return
+		}
+	}
 
+	//If ID isn't found return 404
+	http.Error(writer, "DENIED_ACCESS", http.StatusNotFound) //404
 }
