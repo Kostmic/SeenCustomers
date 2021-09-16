1ddefdef061c2bc3257c02b855d6729f061806ba
Kostmic <44363586+Kostmic@users.noreply.github.com>
2021-09-16 10:02:01 +0200

    Remove loading spinner && cleanup imports and table logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
 import DataTable from "./Datatable";
 
 function App() {
-  useEffect(() => {
-    const url = "http://localhost:8080/users";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  });
   return (
     <div className="App">
       <header className="App-header">
--- frontend/src/Datatable.tsx
+++ frontend/src/Datatable.tsx
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-
-const SORT_ASC = "asc";
 
 const DataTable = ({ fetchUrl, columns }) => {
   const [tableData, setTableData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const loaderStyle = { width: "4rem", height: "4rem", color: "red" };
-  const [sortField, setSortField] = useState(columns[0]);
-  const [sortOrder, setSortOrder] = useState(SORT_ASC);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
 
   useEffect(() => {
     const fetchData = async () => {
-      const params = {
-        sort_field: sortField,
-        sort_order: sortOrder,
-        per_page: perPage,
-        page: currentPage,
-      };
-
       const data = await axios.get(fetchUrl);
       setTableData(data.data);
     };
 
     fetchData();
-  }, [sortField, sortOrder, perPage, currentPage]);
+  });
 
   return (
     <table className="table">
@@ -45,15 +30,7 @@ const DataTable = ({ fetchUrl, columns }) => {
             <td colSpan={columns.length}>No Items found</td>
           </tr>
         ) : null}
-        {loading ? (
-          <tr>
-            <td colSpan={columns.length + 1}>
-              <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-              </div>
-            </td>
-          </tr>
-        ) : (
+        {(
           tableData.map((data) => {
             return (
               <tr key={data}>
